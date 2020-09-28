f8b52d1413267c22f6ed03910f8fe7ad1d2ce679
Ben Stanfield <bnstnfld@gmail.com>
2020-09-27 22:03:24 -0700

    getting there...

--- index.js
+++ index.js
@@ -2,26 +2,6 @@ const imgixParams = (height) => `?auto=compress,format&h=${height || 700}`;
 let carouselCount = 0;
 let carouselInterval;
 
-
-const simplePause = () => {
-  var videoPlayer = document.getElementById('simple-pause');
-  if (!videoPlayer.paused) {
-    document.getElementById("play-button").style.display = "none";
-  }
-  // Play / pause.
-  videoPlayer.addEventListener('click', function () {
-    if (videoPlayer.paused == false) {
-      videoPlayer.pause();
-      document.getElementById("play-button").style.display = "block";
-      videoPlayer.firstChild.nodeValue = 'Play';
-    } else {
-      videoPlayer.play();
-      document.getElementById("play-button").style.display = "none";
-      videoPlayer.firstChild.nodeValue = 'Pause';
-    }
-  });
-}
-
 const toggleCarousel = () => {
   const carousel = document.getElementById('carousel');
   if (carouselCount > 4) {
